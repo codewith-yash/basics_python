8125320dffb77ebdf7c4224a60e3ed9316e10785
codewith-yash <120308159+codewith-yash@users.noreply.github.com>
2022-12-12 12:25:07 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -7,3 +7,19 @@ description :- code regarding star pattern in python
 # how to print single star
 print('*')
 # output :- *
+
+# how to print multiple star in single row
+a = ('* ')
+for i in range(4):
+    print(a ,end = "")
+# output :- * * * *
+
+# how to print multiple star in multiple row
+a = ('*')
+for i in range(4):
+    print(a)
+
+#output :-      *
+#               *
+#               *
+#               *
