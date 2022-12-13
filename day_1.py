63de53fc5958adecb0c8729c75b8bc5f8b99250e
codewith-yash <120308159+codewith-yash@users.noreply.github.com>
2022-12-13 11:34:39 +0530

    Add files via upload

---
+++ day_1.py
@@ -0,0 +1,46 @@
+'''
+author : yashvant pawar
+date : 22/11/2022
+'''
+# introduction of python
+
+# at the starting of learning python first we saw how to declear variable.
+
+# first we have to understand what is variable and how it works.
+
+# variable is a id that store value 
+# every variable has their own\ different memory location 
+
+
+A = 10
+# here i declear A as variable for value of 10 
+# now we declear some more
+
+B = 'yashvant'
+C = 10.5
+D = 'pawar'
+
+# now we print output on console screen
+print(A)                                   # output :- 10
+print(B)                                   # output :- yashvant
+print(A , B)                               # output :- 10 yashvant
+
+# python is case sensitive language if we change case of varible it can't perform any operation on it 
+
+# here i declear variable of C and print c lets see error
+C = "arjun"
+print(c)                                   # output :- NameError: name 'c' is not defined
+
+# now make a short program declear seprate variable on each value and print in same line and different line
+first_name = 'yashvant'
+middle_name = "rajendra"
+last_name = 'pawar'
+
+print(first_name , middle_name , last_name)  # output :- yashvant rajendra pawar
+print('yashvant\nrajendra\npawar')           # output :- yashvant
+#                                                        rajendra
+#                                                        pawar
+
+# here we saw how to assign value to a perticular varible,
+# how to print a variable and perform different operation
+
